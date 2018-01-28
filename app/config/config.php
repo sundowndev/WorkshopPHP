d352c8e72caa17b17aeb703311d73172b2beeb20
Raphael <SundownDEV@users.noreply.github.com>
2018-01-28 19:13:37 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,16 @@
 // define permet de définir une constante
 define('ROOT', $_SERVER['SCRIPT_FILENAME']);
 define('WEBROOT', ROOT . '/public');
+
+// Chemins
 define('DIR_ASSETS', WEBROOT . '/assets');
+define('DIR_APP', ROOT . '/app');
+define('DIR_CONFIG', DIR_APP . '/config');
+define('DIR_MODELS', ROOT . '/models');
+
+// meta
 define('SITE_NAME', 'Mon site lol');
+
+include(DIR_CONFIG . '/db.php');
+
+// connexion à la bdd
