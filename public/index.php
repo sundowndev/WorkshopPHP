e6b7d88d342e01e91b2cfd551723fb5dcd6190a0
Raphael <SundownDEV@users.noreply.github.com>
2018-01-28 18:09:48 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
+<?php
+  include( APP . '/db.php');
+  include( APP . '/config.php');
 
+  // include( MODELS . '/create_article.php');
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?= SITE_NAME ?></title>
+</head>
+<body>
+    <!-- body -->
+</body>
+</html>
